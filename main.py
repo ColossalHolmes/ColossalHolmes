c8a8deb9ed8e5ebba8fbcb7e30ad3aacfe758ec4
COLOSSAL-HOME\Admin <nguyentran02041997@gmail.com>
2022-05-04 23:44:54 +0700

    update

--- main.py
+++ main.py
@@ -11,15 +11,16 @@ def main():
         raw_data = xmltodict.parse(data)
         json_data = json.loads(json.dumps(raw_data))
         returned_data = {
-            'gold_type': json_data['root']['ratelist']['city'][0]['item'][0]['@type'],
+            "gold_type": json_data['root']['ratelist']['city'][0]['item'][0]['@type'],
             "date": json_data['root']['ratelist']['@updated'],
             "buy_price": json_data['root']['ratelist']['city'][0]['item'][0]['@buy'],
             "sell_price": json_data['root']['ratelist']['city'][0]['item'][0]['@sell']
         }
+        output_string = " Gia {0} ngay {1}: Mua vao {2} - Ban ra {3}".format(returned_data['gold_type'], returned_data['date'], returned_data['buy_price'], returned_data['sell_price'])
     except:
         pass
 
-    print(f"::set-output name=myOutput::{returned_data}")
+    print(f"::set-output name=myOutput::{output_string}")
 
 
 if __name__ == "__main__":
